a5d8e84fb918c1cdcec7bdd82d68de69d3863138
Henrik Loeser <data-henrik@users.noreply.github.com>
2022-06-07 11:49:21 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,12 +2,16 @@
 
 set -ex
 
-# IBM Cloud login to target resource group and region
-# The API key is taken from the environment
-ibmcloud login  -g ${ICRESOURCEGROUP} -r ${ICREGION}
+# if CE_DATA is present, inject key/value pairs as env variables
+# needed to pass in, e.g., CE_MIN_SCALE from cron event
+if [ "$CE_DATA" ]; then
+    for s in $(echo "${CE_DATA}" | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
+        export $s
+    done
+fi
 
-# Select the Code Engine project
-ibmcloud ce project select --name ${CEPROJECT}
-# Update the app by seting min-scale to the new value
-ibmcloud ce app update --name ${CEAPP} --min-scale ${CEMINSCALE}
+# IBM Cloud login and data upload to COS
+ibmcloud login  -g ${IC_RESOURCE_GROUP} -r ${IC_REGION}
+ibmcloud ce project select --name ${CE_PROJECT}
+ibmcloud ce app update --name ${CE_APP} --min-scale ${CE_MIN_SCALE}
 exit 0
